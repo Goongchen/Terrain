3c7b7e888af8bb8c794dc6ce0c3f2c328d3e7ae6
Goongchen <107461697+Goongchen@users.noreply.github.com>
2022-06-16 16:00:08 +0530

    Initial commit

---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public string scene;
+    public char _char;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("this is startfunction");
+        Name();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update Funtion");
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene("terrain");
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            SceneManager.LoadScene("Sample");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void Awake()
+    {
+        Debug.Log("Awake function");
+    }
+
+    void Name()
+    {
+        print("My name is Raaj");
+    }
+
+
+
+}
---
+++ Assets/Unity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unity : MonoBehaviour
+{
+    //this is a public class
+    SceneSwitch _sceneswitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
